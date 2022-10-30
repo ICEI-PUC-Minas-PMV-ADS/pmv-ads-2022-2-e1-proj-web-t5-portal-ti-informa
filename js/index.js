9bf082b7b191506bff2d283349fd7ef86784f320
Lizane Rodrigues <lizane.f.rodrigues94@gmail.com>
2022-10-30 17:32:25 -0300

    Atualização para carregar notícias pré carregadas conforme template de cadastro divulgado no ambiente

---
+++ js/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    carregarNoticias();
+
+})
+
+function renderizarNoticia(noticia, index) {
+    let templateNoticiaHtml = $("#templateNoticiaHtml").html();
+    $("#noticiasSemDestaque").append(templateNoticiaHtml);
+    var templateNoticia = $("#noticiasSemDestaque").children("#templateNoticia");
+
+    let tituloNoticia = templateNoticia.children("#tituloNoticia");
+    let imagemNoticia = templateNoticia.children("#imgNoticia");
+    let resumoNoticia = templateNoticia.children("#resumoNoticia");
+
+    tituloNoticia.html(noticia.tituloNoticia);
+    imagemNoticia.attr("src", noticia.imagem)
+    templateNoticia.attr("id", "templateNoticia" + index.toString())
+    resumoNoticia.html(noticia.resumo);
+}
+
+function renderizarNoticiaDestaque(noticia, index) {
+    let templateNoticiaDestaqueHtml = $("#templateNoticiaDestaqueHtml").html();
+    $("#noticiaEmDestaque").append(templateNoticiaDestaqueHtml);
+    var templateNoticiaDestaque = $("#noticiaEmDestaque").children("#templateNoticiaDestaque");
+
+    let tituloNoticia = templateNoticiaDestaque.find(tituloNoticiaDestaque);
+    let imagemNoticia = templateNoticiaDestaque.find(imgNoticiaDestaque);
+    let resumoNoticia = templateNoticiaDestaque.find(resumoNoticiaDestaque);
+
+    tituloNoticia.html(noticia.tituloNoticia);
+    imagemNoticia.attr("src", noticia.imagem)
+    templateNoticiaDestaque.attr("id", "templateNoticiaDestaque" + index.toString())
+    resumoNoticia.html(noticia.resumo);
+}
+
+function carregarNoticias() {
+    let noticias = JSON.parse(localStorage.getItem("noticias"));
+
+    if (!noticias) {
+        noticias = noticiaspreCarregadas;
+    }
+
+    noticias.data.forEach(noticia => {
+        if (noticia.destaque == "não") {
+            renderizarNoticia(noticia, noticias.data.indexOf(noticia));
+        }
+        else {
+            renderizarNoticiaDestaque(noticia, noticias.data.indexOf(noticia));
+        }
+    });
+}
---
+++ js/noticiasPreCarregadas.js
@@ -0,0 +1,44 @@
+var noticiaspreCarregadas = {
+    "data": [
+        {
+            "id": 1,
+            "tituloNoticia": ".NET lança nova versão",
+            "resumo": "O incentivo ao avanço tecnológico, assim como a estrutura atual da organização pode nos levar a considerar a reestruturação do processo de comunicação como um todo.",
+            "imagem": "images/net.png",
+            "conteudo": "O incentivo ao avanço tecnológico, assim como a estrutura atual da organização pode nos levar a considerar a reestruturação do processo de comunicação como um todo. <br> O incentivo ao avanço tecnológico, assim como a estrutura atual da organização pode nos levar a considerar a reestruturação do processo de comunicação como um todo.",
+            "destaque": "sim"
+        },  
+        {
+            "id": 2,
+            "tituloNoticia": "Importância de se atualizar",
+            "resumo": "A certificação de metodologias que nos auxiliam a lidar com a expansão dos mercados mundiais acarreta um processo de reformulação e modernização do sistema de participação geral.",
+            "imagem": "images/mercado-ti.jpg",
+            "conteudo": "A certificação de metodologias que nos auxiliam a lidar com a expansão dos mercados mundiais acarreta um processo de reformulação e modernização do sistema de participação geral. <br> A certificação de metodologias que nos auxiliam a lidar com a expansão dos mercados mundiais acarreta um processo de reformulação e modernização do sistema de participação geral.",
+            "destaque": "não"
+        },  
+        {
+            "id": 3,
+            "tituloNoticia": "JavaScript nova versão",
+            "resumo": "É claro que a contínua expansão de nossa atividade afeta positivamente a correta previsão dos índices pretendidos.",
+            "imagem": "images/javascript.png",
+            "conteudo": "É claro que a contínua expansão de nossa atividade afeta positivamente a correta previsão dos índices pretendidos. <br> É claro que a contínua expansão de nossa atividade afeta positivamente a correta previsão dos índices pretendidos.",
+            "destaque": "não"
+        },  
+        {
+            "id": 4,
+            "tituloNoticia": "Amazon com problemas",
+            "resumo": "É claro que a contínua expansão de nossa atividade afeta positivamente a correta previsão dos índices pretendidos.",
+            "imagem": "images/aws.png",
+            "conteudo": "É importante questionar o quanto a execução dos pontos do programa talvez venha a ressaltar a relatividade do sistema de formação de quadros que corresponde às necessidades. <br> É importante questionar o quanto a execução dos pontos do programa talvez venha a ressaltar a relatividade do sistema de formação de quadros que corresponde às necessidades.",
+            "destaque": "não"
+        },  
+        {
+            "id": 5,
+            "tituloNoticia": "Microsoft x Netflix",
+            "resumo": "É claro que a contínua expansão de nossa atividade afeta positivamente a correta previsão dos índices pretendidos.",
+            "imagem": "images/netflix-microsoft.jpg",
+            "conteudo": "Ainda assim, existem dúvidas a respeito de como a execução dos pontos do programa aponta para a melhoria dos índices pretendidos. <br> Ainda assim, existem dúvidas a respeito de como a execução dos pontos do programa aponta para a melhoria dos índices pretendidos.",
+            "destaque": "não"
+        }            
+    ]
+}
