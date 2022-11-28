a0fb1e447746e361c9863cc6cc05d114699c6681
danisouzapuc <daniellasouza@yahoo.com.br>
2022-11-27 22:17:42 -0300

    Atualização
    Atualização

--- bdcursos.js
+++ bdcursos.js
@@ -1,16 +1,29 @@
-var db = { 
-    dados: [
-        {
-            titulo: 'Cursos Gratuito',
-            imagem: 'http://lorempixel.com.br/200/120/people/1',
-            descrição: 'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.In elementis mé pra quem é amistosis quis leo.Si num tem leite então bota uma pinga aí cumpadi!'
-        },
-        {
-            titulo: 'Cursos Superior',
-            imagem: 'http://lorempixel.com.br/200/120/people/2  ',
-            descrição: 'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.In elementis mé pra quem é amistosis quis leo.Si num tem leite então bota uma pinga aí cumpadi!'
-        }
+const button = document.querySelector('#like');
+const number = document.querySelector('#number');
 
-]
-        
-}
+button.addEventListener('click', () => {
+  let likeValue = document.querySelector('#number').textContent;
+  let newValue = Number(likeValue) + 1;
+  button.classList.add('like');
+  number.innerHTML = newValue;
+})
+
+const button02 = document.querySelector('#like02');
+const number02 = document.querySelector('#number02');
+
+button02.addEventListener('click', () => {
+  let likeValue = document.querySelector('#number02').textContent;
+  let newValue = Number(likeValue) + 1;
+  button02.classList.add('like02');
+  number02.innerHTML = newValue;
+})
+
+const button03 = document.querySelector('#like03');
+const number03 = document.querySelector('#number03');
+
+button03.addEventListener('click', () => {
+  let likeValue = document.querySelector('#number03').textContent;
+  let newValue = Number(likeValue) + 1;
+  button03.classList.add('like03');
+  number03.innerHTML = newValue;
+})
